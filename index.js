058500201064b26513098c093b338613a5d614af
CarrumDraws <calumchansy@gmail.com>
2023-07-24 17:53:45 -0700

    Implememted Image Storage + Some Routes

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ const userRoutes = require("./routes/users.js");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const pool = require("./db.js");
+
+const multer = require("multer");
+// storage: Multer Config
+const storage = multer.diskStorage({
+  // destination: Directory where files are saved.
+  // __dirname = C:\Users\Carrum\ReactStuff\channotes_server
+  destination: (req, file, callback) => {
+    callback(null, __dirname + "/uploads");
+  },
+  // filename: Name the files.
+  filename: (req, file, callback) => {
+    // Naming this way prevents file overlap!
+    callback(null, Date.now() + file.originalname);
+  },
+});
+const uploads = multer({ storage: storage });
 
 // MIDDLEWARE -----
 app.use(express.json()); // Allows access to req.body
@@ -18,6 +36,59 @@ app.use(cors()); // Allows different-domain app interaction
 app.use(bodyParser.json()); // Parse the JSON request body
 dotenv.config(); // Reading .env files
 
+// FILE STORAGE EXAMPLES -----
+app.post("/photo", uploads.single("single"), function (req, res, next) {
+  // .single means "One Photo with Key 'single'"
+  console.log(req.file); // File Data
+  //   console.log(req.file.path); // File Path
+  res.json({ status: "Single File Recieved" });
+});
+
+app.post("/photos", uploads.array("multiple"), function (req, res, next) {
+  console.log(req.files); // File Data
+  res.json({ status: "Multiple Files recieved" });
+});
+
+// ROUTES WITH IMAGE UPLOAD -----
+
+// Takes in data, bcrypt's google_id, Saves to Postgre, returning user_data
+// Also creates "Folders" Folder using your chanID.
+// NOTE: Treats google_id like a password (bcrypts google_id)...
+// ...and treats email like a username.
+app.post("/auth/signup", uploads.single("image"), async (req, res, next) => {
+  try {
+    const { google_id, first_name, last_name, username, email } = req.body;
+
+    if (!google_id || !first_name || !last_name || !username || !email)
+      return res.status(400).send("Missing Data");
+
+    // bcrypt google_id
+    const salt = await bcrypt.genSalt();
+    const googleHash = await bcrypt.hash(google_id, salt);
+
+    // Save User to Postgre
+    let user = await pool.query(
+      "INSERT INTO users (google_id, first_name, last_name, username, email, url) VALUES (($1), ($2), ($3), ($4), ($5), ($6)) RETURNING *;",
+      [googleHash, first_name, last_name, username, email, req.file.path]
+    );
+    user = user.rows[0];
+
+    // Create Homepage Folder
+    let date = new Date();
+    let time = date.toISOString().slice(0, 19).replace("T", " ");
+    await pool.query(
+      "INSERT INTO folders (chan_id, folder_id, title, date_created) VALUES (($1), ($2), ($3), ($4));",
+      [user.chan_id, null, "Your Folders", time]
+    );
+
+    res.send(user);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+// ROUTES -----
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
 app.use("/folders", foldersRoutes);
--- routes/auth.js
+++ routes/auth.js
@@ -2,65 +2,10 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
-// Takes in data, bcrypt's google_id, Saves to Postgre
-// Returns JWT + User Data using google_id
-// Also creates "Folders" Folder using your chanID.
-
-// Note: Treats google_id like a password (bcrypts google_id)...
-// ...and treats email like a username.
-router.post(
-  "/signup",
-  async (req, res, next) => {
-    try {
-      const { google_id, first_name, last_name, username, email, url } =
-        req.body;
-
-      if (
-        !google_id ||
-        !first_name ||
-        !last_name ||
-        !username ||
-        !email ||
-        !url
-      )
-        return res.status(400).send("Missing Data");
-
-      // bcrypt google_id
-      const salt = await bcrypt.genSalt();
-      const googleHash = await bcrypt.hash(google_id, salt);
-
-      // Save User to Postgre
-      let user = await pool.query(
-        "INSERT INTO users (google_id, first_name, last_name, username, email, url) VALUES (($1), ($2), ($3), ($4), ($5), ($6)) RETURNING *;",
-        [googleHash, first_name, last_name, username, email, url]
-      );
-      user = user.rows[0];
-      req.query = { google_id: google_id, email: user.email }; // Store User in query for returnJWT()
-
-      // Create Homepage Folder
-      let date = new Date();
-      let time = date.toISOString().slice(0, 19).replace("T", " ");
-      let folder = await pool.query(
-        "INSERT INTO folders (chan_id, folder_id, title, date_created) VALUES (($1), ($2), ($3), ($4)) RETURNING *;",
-        [user.chan_id, null, "Your Folders", time]
-      );
-
-      next(); // returnJWT();
-    } catch (err) {
-      console.log(err);
-      res.send(err);
-    }
-  },
-  returnJWT
-);
 
 // Validates google_id + email + Returns JWT of chanID
 // If no user found, returns nothing.
-router.get("/hasuser", returnJWT);
-
-async function returnJWT(req, res) {
+router.get("/hasuser", async (req, res) => {
   try {
     const { email, google_id } = req.query;
     if (!email || !google_id) return res.status(400).send("Missing Data");
@@ -76,12 +21,11 @@ async function returnJWT(req, res) {
 
     const token = jwt.sign({ chan_id: user.chan_id }, process.env.JWT_SECRET);
     delete user.google_id;
-    // delete user.chan_id;
     res.status(200).json({ token, user });
   } catch (err) {
     console.log(err);
     res.send(err);
   }
-}
+});
 
 module.exports = router;
